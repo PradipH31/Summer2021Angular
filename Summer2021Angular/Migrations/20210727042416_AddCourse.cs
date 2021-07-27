6a39147bd4b1050d70ea15330ac8091c22af9cb5
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 21:24:51 -0700

    Add Migration for Course model

---
+++ Summer2021Angular/Migrations/20210727042416_AddCourse.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Summer2021Angular.Migrations
+{
+    public partial class AddCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CourseDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CourseInstructor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course");
+        }
+    }
+}
