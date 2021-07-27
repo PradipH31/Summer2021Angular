2d6916e68ed67800af42f41455797719b5f2c3c9
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 21:28:29 -0700

    Add Notebook Model

---
+++ Summer2021Angular/Models/Courses/Notebook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Summer2021Angular.Models.Courses
+{
+    public class Notebook
+    {
+        public int NotebookId { get; set; }
+        public string GithubLink { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CourseId { get; set; }
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedDate { get; set; }
+        [NotMapped]
+        public string JupyterHubLink { get; set; }
+    }
+}
