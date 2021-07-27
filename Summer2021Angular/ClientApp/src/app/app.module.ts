f465b990df322175295c9b6c985177c7bd28d393
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 22:05:26 -0700

    Add Angular Material

--- Summer2021Angular/ClientApp/src/app/app.module.ts
+++ Summer2021Angular/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StudentHomeComponent } from './components/student/student-home/student-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { StudentHomeComponent } from './components/student/student-home/student-
     StudentHomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
