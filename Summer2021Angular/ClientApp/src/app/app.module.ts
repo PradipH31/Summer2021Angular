f1669107067e5283e3106f6bb58e27fb1d5938df
pradiph31 <pradip.hamal31@gmail.com>
2021-07-26 21:59:00 -0700

    Imported and used student-home component

--- Summer2021Angular/ClientApp/src/app/app.module.ts
+++ Summer2021Angular/ClientApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentHomeComponent } from './components/student/student-home/student-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentHomeComponent
   ],
   imports: [
     BrowserModule
