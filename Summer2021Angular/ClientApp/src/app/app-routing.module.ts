e13fcc9f4d90aa221abe408487119c76d7b99a1f
pradiph31 <pradip.hamal31@gmail.com>
2021-07-31 11:33:59 -0700

    Added route for Courses

--- Summer2021Angular/ClientApp/src/app/app-routing.module.ts
+++ Summer2021Angular/ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { StudentHomeComponent } from './components/student/student-home/student-home.component';
 
-
+const routes: Routes = [
+  { path: 'courses', component: StudentHomeComponent }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
